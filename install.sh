0bdadbfca1e68dbfe424c65900b91f2aedd8f699
Winston <bk0515299@gmail.com>
2026-01-20 19:00:36 -0800

    Update install.sh to install 'rb' in /usr/local/bin
    Change installation path for 'rb' script to /usr/local/bin and make it executable.

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 sudo echo "Installing..."
-echo "#!/bin/sh" | sudo tee /usr/bin/rb > /dev/null
+echo "#!/bin/sh" | sudo tee /usr/local/bin/rb > /dev/null
 echo "echo Rebooting..." | sudo tee -a /usr/local/bin/rb > /dev/null
 echo "sudo reboot" | sudo tee -a /usr/local/bin/rb > /dev/null
+chmod +x /usr/local/bin/rb
 echo "----------------------------"
 echo "----------Winston-----------"
 echo "----------------------------"
